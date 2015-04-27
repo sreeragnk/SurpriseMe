a441a3137ac1051d3b4d1d0a000c6897c4924aaa
sreeragnk <sreeragnk@gmail.com>
2015-04-27 15:30:12 +0530

    Initial commit  -- Login Screen -- User Registration completed.

---
+++ app/src/main/java/com/sparklingspur/sreeragnampoothiri/surpriseme/AppConfig.java
@@ -0,0 +1,12 @@
+package com.sparklingspur.sreeragnampoothiri.surpriseme;
+
+/**
+ * Created by sreerag.nampoothiri on 27-Apr-15.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://sparklingspur.com/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://sparklingspur.com/android_login_api/";
+}
